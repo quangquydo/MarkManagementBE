ce98f1adbc4f08e81e8543f6fddbb9ec4da8dd0e
HanYiAn <tobobachu@gmail.com>
2021-05-25 17:27:03 +0700

    first-project

---
+++ src/main/java/com/example/MarkManagementPE/MarkManagementPeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarkManagementPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkManagementPeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkManagementPeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarkManagementPE/MarkManagementPeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarkManagementPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkManagementPeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
